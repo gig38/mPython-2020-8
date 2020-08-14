9090e1f9259e6ad908358174f4861e91e8fd2c2d
gig38 <69449396+gig38@users.noreply.github.com>
2020-08-14 15:10:03 +0800

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,34 @@
+from mcpi.minecraft import Minecraft
+import random
+mc = Minecraft.create()
+x,y,z = mc.player.getTilePos()
+for i in range(20):
+    r = random.randrange(1,7)
+   
+    if r == 1:
+        mc.setBlocks(x,y,z,x+4,y,z,1)
+        x = x+4
+        
+    elif r == 2:
+        mc.setBlocks(x,y,z,x-4,y,z,17)
+        x = x-4
+        
+    elif r == 3:
+        mc.setBlocks(x,y,z,x,y,z+4,41)
+        z = z+4
+        
+    elif r == 4:
+        mc.setBlocks(x,y,z,x,y,z-4,57)
+        z = z-4
+        
+    elif r == 5:
+        mc.setBlocks(x,y,z,x,y+4,z,57)
+        y = y+4
+        
+    elif r == 6:
+        mc.setBlocks(x,y,z,x,y-4,z,168)
+        
+    
+        
+    
+        
